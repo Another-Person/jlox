eb05fc6d98ddd6dffb2f6bfe7025e81a07fa072b
Joey Sachtleben <manjoey7@gmail.com>
2023-10-13 11:41:54 -0500

    Added core of parsing and AST generation

--- src/Lox.java
+++ src/Lox.java
@@ -96,11 +96,19 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        // For now, just print out the tokens
+        /*// For now, just print out the tokens
         for (Token token : tokens)
         {
             System.out.println(token);
-        }
+        }*/
+
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        // Stop if there was a syntax error.
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     /**
@@ -126,5 +134,13 @@ public class Lox {
     {
         System.err.println("[line " + line + "] Error " + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 }
